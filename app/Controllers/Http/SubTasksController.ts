e6b4299988620cdd7f4067dc07e36698d5e075b2
PedroHercules <pedroherculesdantas@gmail.com>
2022-08-08 19:55:24 -0300

    Create routes and CRUD subtasks

--- app/Controllers/Http/SubTasksController.ts
+++ app/Controllers/Http/SubTasksController.ts
@@ -32,4 +32,113 @@ export default class SubTasksController {
       subTask
     });
   }
+
+  public async index({ response, params } : HttpContextContract) {
+
+    const taskId = params.taskId;
+
+    if (isNaN(taskId)) {
+      return response.status(400).json({ message: "Parameters must be a number" });
+    }
+
+    const task = await Task.find(taskId);
+
+    if (!task) {
+      return response.status(404).json({ message: "task not found" });
+    }
+
+    const subTasks = await SubTask.query().where({task_id: taskId});
+
+    if (subTasks.length == 0) {
+      return response.status(404).json({ message: "Subtasks empty" });
+    }
+
+    return response.status(200).json({ subTasks });
+  }
+
+  public async show({ params, response } : HttpContextContract) {
+    const taskId = params.taskId;
+    const subtaskId = params.id;
+
+    if (isNaN(taskId) || isNaN(subtaskId)) {
+      return response.status(400).json({ message: "Parameters must be a number" });
+    }
+
+    const task = await Task.find(taskId);
+
+    if (!task) {
+      return response.status(404).json({ message: "task not found" });
+    }
+
+    const subtask = await SubTask.find(subtaskId)
+
+    if (!subtask) {
+      return response.status(404).json({ message: "Subtask not found" });
+    }
+
+    return response.status(200).json({
+      subtask
+    });
+  }
+
+  public async update({ params, request, response } : HttpContextContract) {
+    const taskId = params.taskId;
+    const subtaskId = params.id;
+
+    if (isNaN(taskId) || isNaN(subtaskId)) {
+      return response.status(400).json({ message: "Parameters must be a number" });
+    }
+
+    const task = await Task.find(taskId);
+
+    if (!task) {
+      return response.status(404).json({ message: "task not found" });
+    }
+
+    const { title } = request.body() as SubTaskType;
+
+    const subtask = await SubTask.find(subtaskId)
+
+    if (!subtask) {
+      return response.status(404).json({ message: "Subtask not found" });
+    }
+
+    if (title !== subtask.title || title != null){
+      subtask.title = title
+      subtask.save()
+    }
+
+    return response.status(200).json({
+      message: "subtask updated successfully",
+      subtask
+    });
+  }
+
+  public async destroy({ params, response } : HttpContextContract) {
+    const taskId = params.taskId;
+    const subtaskId = params.id;
+
+    if (isNaN(taskId) || isNaN(subtaskId)) {
+      return response.status(400).json({ message: "Parameters must be a number" });
+    }
+
+    const task = await Task.find(taskId);
+
+    if (!task) {
+      return response.status(404).json({ message: "task not found" });
+    }
+
+    const subtask = await SubTask.find(subtaskId)
+
+    if (!subtask) {
+      return response.status(404).json({ message: "Subtask not found" });
+    }
+
+    await subtask.delete();
+
+    return response.status(200).json({
+      message: "Subtask deleted successfully",
+      subtask
+    });
+  }
 }
