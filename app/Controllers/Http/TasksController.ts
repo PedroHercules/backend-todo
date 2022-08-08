0e4bf9be9c47c3431809e3a01e47444fd6506dcb
PedroHercules <pedroherculesdantas@gmail.com>
2022-08-08 18:00:24 -0300

    added get task by id

--- app/Controllers/Http/TasksController.ts
+++ app/Controllers/Http/TasksController.ts
@@ -20,8 +20,26 @@ export default class TasksController {
   }
 
   public async index({ response } : HttpContextContract) {
-    const tasks = await Task.query();
+    const tasks = await Task.all();
 
     return response.status(200).json({ tasks });
+  }
+
+  public async show({ params, response } : HttpContextContract) {
+    const id = params.id;
+    
+    if (isNaN(id)) {
+      return response.status(400).json({ message: "Parameter must be a number" });
+    }
+    
+    const task = await Task.find(id)
+    
+    if (!task) {
+      return response.status(404).json({ message: "task not found" });
+    }
+
+    return response.status(200).json({
+      task
+    });
   }
 }
