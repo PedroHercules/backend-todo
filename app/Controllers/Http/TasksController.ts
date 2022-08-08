0d4db40aa11632da85474035a74ce0bd23d0b6d7
PedroHercules <pedroherculesdantas@gmail.com>
2022-08-08 18:23:57 -0300

    Added destroy and update task

--- app/Controllers/Http/TasksController.ts
+++ app/Controllers/Http/TasksController.ts
@@ -22,6 +22,10 @@ export default class TasksController {
   public async index({ response } : HttpContextContract) {
     const tasks = await Task.all();
 
+    if (tasks.length == 0) {
+      return response.status(404).json({ message: "tasks empty" });
+    }
+
     return response.status(200).json({ tasks });
   }
 
@@ -31,7 +35,7 @@ export default class TasksController {
     if (isNaN(id)) {
       return response.status(400).json({ message: "Parameter must be a number" });
     }
-    
+
     const task = await Task.find(id)
     
     if (!task) {
@@ -41,5 +45,60 @@ export default class TasksController {
     return response.status(200).json({
       task
     });
+  }
+
+  public async update({ params, request, response } : HttpContextContract) {
+    const id = params.id;
+    
+    if (isNaN(id)) {
+      return response.status(400).json({ message: "Parameter must be a number" });
+    }
+
+    const task = await Task.find(id)
+    
+    if (!task) {
+      return response.status(404).json({ message: "task not found" });
+    }
+
+    const { title, description, date } = request.body();
+
+    if (title !== task.title && title != null) {
+      task.title = title;
+    }
+    if (description !== task.description && description != null) {
+      task.description = description;
+    }
+    if (date !== task.date && date != null) {
+      task.date = date;
+    }
+
+    await task.save();
+
+    return response.status(200).json({ 
+      message: "Task update successfully",
+      task
+    });
+  }
+
+  public async destroy({ params, response } : HttpContextContract) {
+    const id = params.id;
+    
+    if (isNaN(id)) {
+      return response.status(400).json({ message: "Parameter must be a number" });
+    }
+
+    const task = await Task.find(id)
+    
+    if (!task) {
+      return response.status(404).json({ message: "task not found" });
+    }
+
+    await task.delete();
+
+    return response.status(200).json({
+      message: "Task deleted successfully",
+      task
+    });
+
   }
 }
