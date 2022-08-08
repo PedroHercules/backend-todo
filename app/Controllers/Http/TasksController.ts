da68c9598d9543d6d7bb0730fed816684ac327d1
PedroHercules <pedroherculesdantas@gmail.com>
2022-08-07 21:25:06 -0300

    Create and read tasks

--- app/Controllers/Http/TasksController.ts
+++ app/Controllers/Http/TasksController.ts
@@ -1,9 +1,27 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+import Task from 'App/Models/Task'
+
+
+type taskType = {
+  title: string
+  description: string
+  date: string
+}
 
 export default class TasksController {
-  public async store() {
-    return {
-      msg: "Rota para criar task"
-    }
+  public async store({ request, response } : HttpContextContract) {
+
+    const taskData = request.body() as taskType;
+
+    const task = await Task.create(taskData);
+
+    return response.status(201).json({task})
+  }
+
+  public async index({ response } : HttpContextContract) {
+    const tasks = await Task.query();
+
+    return response.status(200).json({ tasks });
   }
 }
